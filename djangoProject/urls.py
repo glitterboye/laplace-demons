efa77bc0641ca120bcc8f676c680fbf8288f9a77
tim <timothy.tan2023@gmail.com>
2021-03-28 09:40:45 -0400

    added clip art on homepage

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -4,5 +4,6 @@ from django.urls import include, path
 urlpatterns = [
     path('home/', include('webapp.urls')),
     path('nest/', include('webapp.urls')),
+    path('home/#about', include('webapp.urls')),
     path('admin/', admin.site.urls),
 ]
